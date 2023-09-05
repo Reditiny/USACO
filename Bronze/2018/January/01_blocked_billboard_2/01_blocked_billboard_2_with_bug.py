a2ca29b5302e4a45e1c88e7351fbf7ca61e7add1
JohnnyShi <shiyuan1994@gmail.com>
2023-09-05 22:26:08 +0800

    add blocked_billboard_2

---
+++ Bronze/2018/January/01_blocked_billboard_2/01_blocked_billboard_2_with_bug.py
@@ -0,0 +1,27 @@
+"""
+ID: mck15821
+LANG: PYTHON3
+PROG: billboard
+"""
+# http://www.usaco.org/index.php?page=viewproblem2&cpid=783
+fin = open('billboard.in', 'r')
+fout = open("billboard.out", "w")
+
+x1, y1, x2, y2 = map(int, fin.readline().strip().split())  # lawnmower
+x3, y3, x4, y4 = map(int, fin.readline().strip().split())  # billboard
+
+overlap_x = max(0, min(x2, x4) - max(x1, x3))
+overlap_y = max(0, min(y2, y4) - max(y1, y3))
+overlap_area = overlap_x * overlap_y
+lawnmower_area = (x2 - x1) * (y2 - y1)
+billboard_area = (x4 - x3) * (y4 - y3)
+result = lawnmower_area
+
+# minimum area is less than the entire lawnmower billboard area if and only if
+# the overlap's width or length is the same as billboard's length or width
+# issue: missing one condition
+if (overlap_x == x2 - x1 or overlap_y == y2 - y1) and overlap_area > 0:
+    result -= overlap_area
+
+fout.write(f"{result}\n")
+fout.close()
