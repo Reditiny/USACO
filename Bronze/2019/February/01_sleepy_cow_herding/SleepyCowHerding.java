d9ba9baafe38c72ef3182cf1ba6dbc188e44975f
red <erickbin@stu.pku.edu.cn>
2023-11-03 09:37:52 +0800

    fulfil comments of search

--- Bronze/2019/February/01_sleepy_cow_herding/SleepyCowHerding.java
+++ Bronze/2019/February/01_sleepy_cow_herding/SleepyCowHerding.java
@@ -22,11 +22,11 @@ public class SleepyCowHerding {
             cows.add(Integer.parseInt(st.nextToken()));
         }
         cows.sort(Integer::compareTo);
-        int gapB = cows.get(1) - cows.get(0);
-        int gapE = cows.get(2) - cows.get(1);
+        int gap1 = cows.get(1) - cows.get(0);
+        int gap2 = cows.get(2) - cows.get(1);
         // 得到相邻两头牛之间的位置
-        int minGap = Math.min(gapB, gapE) - 1;
-        int maxGap = Math.max(gapB, gapE) - 1;
+        int minGap = Math.min(gap1, gap2) - 1;
+        int maxGap = Math.max(gap1, gap2) - 1;
         // 当 maxGap 较大时 总可以最少两步就放在一起 B___E______M -> E_B____M -> EMB
         // 最多需要 maxGap 步 较短一端的两头牛依次往最近的地方走  B___E______M -> EB_____M -> BE____M -> EB___M -> BE__M -> EB_M -> BEM
         // minGap == 1 时要额外考虑 B_E_____M -> BME 一步
