b5f39f8103490bc3bcb76686777f6d4f12c8e015
red <erickbin@stu.pku.edu.cn>
2024-01-22 22:44:53 +0800

    fulfill bronze_2019_supplement

--- Bronze/2019/February/02_the_great_revegetation/the_great_revegetation.cpp
+++ Bronze/2019/February/02_the_great_revegetation/the_great_revegetation.cpp
@@ -13,7 +13,7 @@ int main() {
     fin >> n >> m;
 
     vector<set<int>> graph(n);
-    vector<int> grasses;
+    vector<int> grasses(n, 0);
     // 记录牧场的边关系(一头牛喜欢的两个牧场之间有条边) 邻接表 有边相连的牧场不能种相同的种子
     for (int i = 0; i < m; i++) {
         int pasture1, pasture2;
@@ -27,24 +27,15 @@ int main() {
         // 对于当前的 i 牧场，查看所有与 i 有边相连的 neighbor 牧场
         for (int neighbor : graph[i]) {
             // grasses[neighbor] 表示 neighbor 牧场种的种子
-            // 如果 neighbor < grasses.size() 说明 neighbor 牧场已经种了种子
-            // 那么就把这个种子加入到当前牧场的不能种的种子集合中
-
-            // 例如 1，2，3 三个牧场，其中 1，2 有边相连，2，3 有边相连
-            // 从前往后依次确定种子
-            // 当前牧场是 1 时，此时 grasses = {} ，因为还没有开始种，所以可以种 1 号种子，种完 grasses = {1}
-            // 然后时牧场 2，此时 grasses = {1}，遍历与 2 相连的牧场 1，3，发现 grasses[1] = 1, 说明 1 号牧场已经种了 1 号种子
-            // 所以只能种 2 号种子，然后 grasses = {1, 2}
-            // 然后是牧场 3，此时 grasses = {1, 2}，遍历与 3 相连的牧场 2，发现 grasses[2] = 2, 说明 2 号牧场已经种了 2 号种子
-            // 所以只能种 1 号种子，然后 grasses = {1, 2, 1}
-            if (neighbor < grasses.size()) {
+            // 0 表示 neighbor 牧场还没有种种子
+            if (grasses[neighbor] != 0) {
                 exist_grasses.insert(grasses[neighbor]);
             }
         }
         // 找到 i 牧场可以种的第一种种子，因为题目有要求按多解时按小序列输出
         for (int j = 1; j <= 4; j++) {
             if (exist_grasses.find(j) == exist_grasses.end()) {
-                grasses.push_back(j);
+                grasses[i] = j;
                 break;
             }
         }
--- Bronze/2019/US Open/02_milk_factory/milk_factory.cpp
+++ Bronze/2019/US Open/02_milk_factory/milk_factory.cpp
@@ -6,31 +6,7 @@
 
 using namespace std;
 
-vector<vector<int>> graph;
-unordered_set<int> target_stations;
-int ans;
-// 从 start 开始进行广度优先搜索
-// 返回值为从 start 开始可以到达的所有工厂
-unordered_set<int> bfs(int start) {
-    unordered_set<int> visited;
-    visited.insert(start);
-    queue<int> q;
-    q.push(start);
-
-    while (!q.empty()) {
-        int cur = q.front();
-        q.pop();
-
-        for (int neighbor : graph[cur]) {
-            if (visited.find(neighbor) == visited.end()) {
-                visited.insert(neighbor);
-                q.push(neighbor);
-            }
-        }
-    }
-
-    return visited;
-}
+vector<vector<int> > graph;
 
 int main() {
     ifstream fin("factory.in");
@@ -38,41 +14,48 @@ int main() {
 
     int n;
     fin >> n;
-    ans = n;
 
     graph.resize(n);
-    for (int i = 0; i < n; i++) {
-        target_stations.insert(i);
-    }
-
+  
+    
     for (int i = 1; i < n; i++) {
         int station1, station2;
         fin >> station1 >> station2;
-        graph[station1 - 1].push_back(station2 - 1);
+        graph[station2 - 1].push_back(station1 - 1);
     }
-    // 对于每个工厂，都进行一次广度优先搜索，得到从该工厂可以到达的所有工厂
-    // 最终的交集即为目标工厂
+    int root = -1;
     for (int i = 0; i < n; i++) {
-        unordered_set<int> reached_station = bfs(i);
-        for (int station : target_stations) {
-            if (reached_station.find(station) == reached_station.end()) {
-                target_stations.erase(station);
+        vector<bool> visited(n);
+        visited[i] = true;
+        // DFS 遍历查看是否能够到达所有的节点
+        vector<int> reached{i};
+        while (!reached.empty()) {
+            int cur = reached.back();
+            reached.pop_back();
+            for (int n: graph[cur]) {
+                if (!visited[n]) {
+                    visited[n] = true;
+                    reached.push_back(n);
+                }
+            }
+        }
+        // 判断是否能够到达所有的节点
+        bool valid = true;
+        for (int check_s = 0; check_s < n; check_s++) {
+            if (!visited[check_s]) {
+                valid = false;
+                break;
             }
         }
 
-        if (target_stations.empty()) {
+        if (valid) {
+            root = i + 1;
             break;
         }
+
     }
 
-    if (target_stations.empty()) {
-        fout << "-1" << endl;
-    } else {
-        for (int station : target_stations) {
-            ans = min(ans, station);
-        }
-        fout << ans + 1 << endl;
-    }
+    fout << root << endl;
 
     fin.close();
     fout.close();
