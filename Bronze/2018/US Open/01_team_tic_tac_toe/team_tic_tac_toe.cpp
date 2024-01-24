6c0e122211e1ac3753fc6b85c18a538f871adf15
red <erickbin@stu.pku.edu.cn>
2024-01-24 21:07:29 +0800

    fulfill bronze_2018_supplement

--- Bronze/2018/US Open/01_team_tic_tac_toe/team_tic_tac_toe.cpp
+++ Bronze/2018/US Open/01_team_tic_tac_toe/team_tic_tac_toe.cpp
@@ -1,63 +1,60 @@
 #include <iostream>
 #include <fstream>
+#include <unordered_set>
 
 using namespace std;
 //  判断给定单牛是否获胜
 bool singleWin(char target, char board[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        // 行
+        if (board[i][0] == target && board[i][1] == target && board[i][2] == target) {
+            return true;
+        }
+        // 列
+        if (board[0][i] == target && board[1][i] == target && board[2][i] == target) {
+            return true;
+        }
+    }
+    // 对角线
     if (board[0][0] == target && board[1][1] == target && board[2][2] == target) {
         return true;
     }
     if (board[0][2] == target && board[1][1] == target && board[2][0] == target) {
         return true;
     }
-    for (int i = 0; i < 3; i++) {
-        if (board[i][0] == target && board[i][1] == target && board[i][2] == target) {
-            return true;
-        }
-        if (board[0][i] == target && board[1][i] == target && board[2][i] == target) {
-            return true;
-        }
-    }
     return false;
+}
+// 双牛获胜排列
+bool check_if_winners(char target1, char target2, char x, char y, char z) {
+    unordered_set<char> s;
+    s.insert(x);
+    s.insert(y);
+    s.insert(z);
+    return s.size() == 2 && s.count(target1) && s.count(target2);
 }
 //  判断给定双牛是否获胜
-bool teamWin(char target1, char target2, char board[3][3]) {
+bool team_win(char ch1, char ch2, char board[3][3]) {
     for (int i = 0; i < 3; i++) {
-        if ((board[i][0] == target1 && board[i][1] == target2 && board[i][2] == target2) ||
-            (board[i][0] == target2 && board[i][1] == target1 && board[i][2] == target2) ||
-            (board[i][0] == target2 && board[i][1] == target2 && board[i][2] == target1) ||
-            (board[i][0] == target2 && board[i][1] == target1 && board[i][2] == target1) ||
-            (board[i][0] == target1 && board[i][1] == target2 && board[i][2] == target1) ||
-            (board[i][0] == target1 && board[i][1] == target1 && board[i][2] == target2)) {
+        // 行
+        if (check_if_winners(ch1, ch2, board[i][0], board[i][1], board[i][2])) {
             return true;
         }
-        if ((board[0][i] == target1 && board[1][i] == target2 && board[2][i] == target2) ||
-            (board[0][i] == target2 && board[1][i] == target1 && board[2][i] == target2) ||
-            (board[0][i] == target2 && board[1][i] == target2 && board[2][i] == target1) ||
-            (board[0][i] == target2 && board[1][i] == target1 && board[2][i] == target1) ||
-            (board[0][i] == target1 && board[1][i] == target2 && board[2][i] == target1) ||
-            (board[0][i] == target1 && board[1][i] == target1 && board[2][i] == target2)) {
+        // 列
+        if (check_if_winners(ch1, ch2, board[0][i], board[1][i], board[2][i])) {
             return true;
         }
     }
-    if ((board[0][0] == target1 && board[1][1] == target2 && board[2][2] == target2) ||
-        (board[0][0] == target2 && board[1][1] == target1 && board[2][2] == target2) ||
-        (board[0][0] == target2 && board[1][1] == target2 && board[2][2] == target1) ||
-        (board[0][0] == target2 && board[1][1] == target1 && board[2][2] == target1) ||
-        (board[0][0] == target1 && board[1][1] == target2 && board[2][2] == target1) ||
-        (board[0][0] == target1 && board[1][1] == target1 && board[2][2] == target2)) {
+    // 对角线
+    if (check_if_winners(ch1, ch2, board[0][0], board[1][1], board[2][2])) {
         return true;
     }
-    if ((board[0][2] == target1 && board[1][1] == target2 && board[2][0] == target2) ||
-        (board[0][2] == target2 && board[1][1] == target1 && board[2][0] == target2) ||
-        (board[0][2] == target2 && board[1][1] == target2 && board[2][0] == target1) ||
-        (board[0][2] == target2 && board[1][1] == target1 && board[2][0] == target1) ||
-        (board[0][2] == target1 && board[1][1] == target2 && board[2][0] == target1) ||
-        (board[0][2] == target1 && board[1][1] == target1 && board[2][0] == target2)) {
+    if (check_if_winners(ch1, ch2, board[0][2], board[1][1], board[2][0])) {
         return true;
     }
     return false;
 }
+
+
 
 int main() {
     ifstream fin("tttt.in");
@@ -79,7 +76,7 @@ int main() {
         }
         for (char c2 = 'Z'; c2 > c1; c2--) {
             // 双牛获胜  teamWin 中已实现 ZA 和 AZ 为相同组合
-            if (teamWin(c1, c2, board)) {
+            if (team_win(c1, c2, board)) {
                 team_count++;
             }
         }
